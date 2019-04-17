5bf2afae104a42979095bd1e37506ab01ef3e7c5
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-17 13:32:30 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
- 
+ if block_given?
+  i = 0 
+  new_collection = []
+  while i < collection.length
+
+  new_collection[i] = yield(collection[i])
+  i = i + 1
+end
+new_collection
+
+else
+  puts "Hey! No block was given!"
+end
+
+end
 end
